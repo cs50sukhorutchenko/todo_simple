8662561c1b65904627f55d239242deea728dbd58
cs50sukhorutchenko <cs50sukhorutchenkol@gmail.com>
2019-08-06 14:47:49 +0300

    generate controller Projects and Tasks with methods

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,18 @@
+class ProjectsController < ApplicationController
+  
+  def index #list of projects with tasks
+  end
+
+  def show #specified projiec by id
+  end
+
+  def create #create new projict with params
+  end
+
+  def update #update specified project
+  end
+
+  def delete #delete specified project
+  end
+  
+end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,12 @@
+class TasksController < ApplicationController
+
+  def create # create new task, need to pass project_id
+  end
+
+  def update # update specified task
+  end
+
+  def delete # delete specified task
+  end
+  
+end
