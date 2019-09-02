82f61084e473ccc788dbe1d1242dcea56a0f7de9
cs50sukhorutchenko <cs50sukhorutchenkol@gmail.com>
2019-09-02 12:18:34 +0300

    Work with projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,19 @@
 class ProjectsController < ApplicationController
   
   def index #list of projects with tasks
+    render json: Project.all
   end
 
   def show #specified projiec by id
+    # byebug
+    render json: Project.find(params[:id])
   end
 
   def create #create new projict with params
+    @project = Article.new(project_params)
+
+    @project.save
+    rediredt_to @project
   end
 
   def update #update specified project
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/projects', to: 'projects#index'
+  get '/projects/:id', to: 'projects#show'
+  post '/projects', to: 'projects#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
